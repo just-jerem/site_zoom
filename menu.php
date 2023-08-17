8c60899816e1e77e0f1440fc2c64ec48453f226c
jerem <jeremie.prouteau75@gmail.com>
2023-08-17 19:41:24 +0200

    uppload search engine (en dev)

--- menu.php
+++ menu.php
@@ -1,3 +1,9 @@
+<header class="short-header">
+
+		<div class="gradient-block"></div>
+
+		<div class="row header-content">
+
 <div class="logo">
 			<a href="/">Author</a>
 		</div>
@@ -115,3 +121,28 @@
 		<li><a href="/contact.html" title="">L'équipe</a></li>
 	</ul>
 </nav>
+
+<div class="search-wrap">
+
+				<form role="search" method="get" class="search-form" action="#">
+					<label>
+						<span class="hide-content">Search for:</span>
+						<input type="search" class="search-field" id="search-input" placeholder="Type Your Keywords" value="" name="s"
+							title="Search for:" autocomplete="off">
+					</label>
+					<input type="submit" class="search-submit" value="Search">
+				</form>
+
+				<a href="#" id="close-search" class="close-btn">Close</a>
+
+			</div> <!-- end search wrap -->
+
+			<div class="triggers">
+				<a class="search-trigger" href="#"><i class="fa fa-search"></i></a>
+				<a class="menu-toggle" href="#"><span>Menu</span></a>
+			</div> <!-- end triggers -->
+
+		</div>
+
+	</header> <!-- end header -->
+
